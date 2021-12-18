00e2b3f7506c9d26186cdb7f5806a75cbe7d2aa0
mr-scrpt <mr.scrpt@gmail.com>
2021-12-18 17:57:31 +0200

    implemented: result section

--- src/data/index.js
+++ src/data/index.js
@@ -4,4 +4,5 @@ exports.data = {
 	menu: JSON.parse(readFileSync("./src/data/menu.json", "utf8")),
 	models: JSON.parse(readFileSync("./src/data/models.json", "utf8")),
 	social: JSON.parse(readFileSync("./src/data/social.json", "utf8")),
+	benefits: JSON.parse(readFileSync("./src/data/benefits.json", "utf8")),
 };
--- src/js/custom/sliders.js
+++ src/js/custom/sliders.js
@@ -1,11 +1,18 @@
 $(document).ready(() => {
-	/*reviews*/
+	/*models*/
 	const models = document.querySelector(".models__slider.swiper");
 	const modelsBox = models.closest(".models__inner");
 	const modelsPagination = modelsBox.querySelector(".models__pagination");
-	console.log("-> ee", modelsPagination);
 	const optionModels = getOptionModels(modelsPagination);
 	sliderGen(models, optionModels);
+
+	/* result */
+
+	const result = document.querySelector(".result__slider-box.swiper");
+	const resultBox = result.closest(".result__inner");
+	const resultPagination = resultBox.querySelector(".result__pagination");
+	const optionResult = getOptionResult(resultPagination);
+	sliderGen(result, optionResult);
 });
 
 const sliderGen = (block, option) => {
@@ -28,6 +35,31 @@ const getOptionModels = (paginationEl) => {
 		},
 		breakpoints: {
 			768: {
+				slidesPerView: 2,
+			},
+			994: {
+				slidesPerView: 3,
+			},
+		},
+	};
+};
+
+const getOptionResult = (paginationEl) => {
+	return {
+		slidesPerView: 1,
+		/*initialSlide: 1,*/
+		/*loop: true,*/
+		/*centeredSlides: true,*/
+		spaceBetween: 30,
+		/*autoHeight: true,*/
+		navigation: false,
+		pagination: {
+			el: paginationEl,
+			type: "bullets",
+			clickable: true,
+		},
+		breakpoints: {
+			560: {
 				slidesPerView: 2,
 			},
 			994: {
